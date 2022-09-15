0b84f8a76860b0f6231d332fa2d5f9fdc46538e8
HelloTanvir <tanvirhossain.butex@gmail.com>
2022-09-15 14:42:50 +0600

    logo size changed

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,7 +1,7 @@
 import Image from 'next/image';
 
 function logo() {
-    return <Image src="/logo.svg" width={80} height={40} alt="logo" />;
+    return <Image src="/logo.svg" width={40} height={40} alt="logo" />;
 }
 
 export default logo;
