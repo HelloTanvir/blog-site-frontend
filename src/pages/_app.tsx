802e3cb194da675e5f6741420c766fa53b2603cc
HelloTanvir <tanvirhossain.butex@gmail.com>
2022-09-17 09:16:43 +0600

    apollo client provider configured

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,18 +1,22 @@
+import { ApolloProvider } from '@apollo/client';
 import { ThemeProvider } from 'next-themes';
 import type { AppProps } from 'next/app';
-import { Provider } from 'react-redux';
+import { Provider as StoreProvider } from 'react-redux';
+import client from '../apollo/client';
 import Layout from '../components/Layout';
 import store from '../store/store';
 import '../styles/globals.css';
 
 export default function App({ Component, pageProps }: AppProps) {
     return (
-        <Provider store={store}>
-            <ThemeProvider enableSystem attribute="class">
-                <Layout>
-                    <Component {...pageProps} />
-                </Layout>
-            </ThemeProvider>
-        </Provider>
+        <ApolloProvider client={client}>
+            <StoreProvider store={store}>
+                <ThemeProvider enableSystem attribute="class">
+                    <Layout>
+                        <Component {...pageProps} />
+                    </Layout>
+                </ThemeProvider>
+            </StoreProvider>
+        </ApolloProvider>
     );
 }
