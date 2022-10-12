757f67f47d5b44e2f7d24f5adc467f4efcf5699e
HelloTanvir <tanvirhossain.butex@gmail.com>
2022-10-12 15:31:32 +0600

    set up file upload environment

--- src/apollo/client.ts
+++ src/apollo/client.ts
@@ -1,6 +1,10 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
+import { createUploadLink } from 'apollo-upload-client';
 
 export const client = new ApolloClient({
-    uri: process.env.NEXT_PUBLIC_API_URL,
+    // uri: process.env.NEXT_PUBLIC_API_URL,
     cache: new InMemoryCache(),
+    link: createUploadLink({
+        uri: process.env.NEXT_PUBLIC_API_URL,
+    }),
 });
