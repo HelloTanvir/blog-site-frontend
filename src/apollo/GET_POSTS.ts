177a1a71dfe44973ea62e6d0fd88e67a52646d72
HelloTanvir <tanvirhossain.butex@gmail.com>
2022-10-12 15:31:45 +0600

    query bug fixed

--- src/apollo/GET_POSTS.ts
+++ src/apollo/GET_POSTS.ts
@@ -1,7 +1,7 @@
 import { gql } from '@apollo/client';
 
 export const GET_POSTS = gql`
-    query GetPosts($input: PostGetDto) {
+    query GetPosts($input: PostGetDto!) {
         posts(getPostInput: $input) {
             id
             title
